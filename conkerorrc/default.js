62f5315ecc1dd76930be9622f0e6799c58f75420
Gregoire Seux <g.seux@criteo.com>
2013-08-12 14:14:16 +0000

    Add conkerorrc

---
+++ conkerorrc/default.js
@@ -0,0 +1,30 @@
+//Default file with basic handling
+dumpln("Toto"); //it sees it has never really worked
+
+
+//Learn something new !
+homepage = "http://en.wikipedia.org/wiki/Special:Random"
+
+//Omnibox style for the mini buffer
+minibuffer_auto_complete_default = true;
+url_completion_use_history = true;
+
+//Bindings
+//function scroll_verti_complete (buffer, n) {
+//    vqr w = buffer.focused_frame;
+//    w.scrollTo (n > 0 ? w.scrollMaxY : 0, w.scrollX);
+//}
+//interactive("scroll-end-of-page",
+//    "Scroll to the end the page",
+//    function (I) { scroll_verti_complete(I.buffer, 1 });
+//define_key("default_global_keymap", "H", "scroll-end-of-age")
+
+
+//Page modes
+
+require("xkcd");
+xkcd_add_title = true;
+
+require("youtube"); //I should set a vlc handler instead
+
+require("smbc");
