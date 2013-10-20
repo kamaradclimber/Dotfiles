2c02371ce36444479adcc6fa531139b2bd067bf0
Gregoire Seux <g.seux@criteo.com>
2013-10-20 11:10:23 +0200

    Try to user cokkieculler in conkeror

--- conkerorrc/default.js
+++ conkerorrc/default.js
@@ -71,3 +71,17 @@ key_kill_mode.test.push(/\/\/.*facebook\..*\//);
 //      //if (result != 0 || error != "")
 //      //    throw new interactive_error("status "+result+", "+error);
 //  });
+
+const cookie_culler_chrome = "chrome://cookieculler/content/CookieCuller.xul";
+
+interactive("cookie-culler-dialog", "Show the CookieCuller settings in a dialog box.",
+    function (I) {
+        var frame = I.buffer.top_frame;
+        frame.openDialog(cookie_culler_chrome,
+                         "CookieCuller",
+                         "centerscreen,chrome,dialog,modal,resizable");
+    });
+
+interactive("cookie-culler", "Open the CookieCuller settings in a new buffer.",
+    "find-url-new-buffer",
+    $browser_object = cookie_culler_chrome);
