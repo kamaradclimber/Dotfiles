fecae34deb2f543edc6628a791de88ef5e6681bb
Gregoire Seux <g.seux@criteo.com>
2013-11-15 16:10:50 +0000

    Whitespace change

--- xmonad.hs
+++ xmonad.hs
@@ -122,7 +122,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList [
     ((modm, button1), \w -> focus w >> mouseMoveWindow w
                                        >> windows W.shiftMaster),
     -- mod-button2, Raise the window to the top of the stack
-    ((modm, button2), \w -> focus w >> windows W.shiftMaster),
+    ((modm, button2), \w -> focus w >> windows W.shiftMaster)
     -- mod-button3, Set the window to floating mode and resize by dragging
     --, ((modm, button3), (\w -> focus w >> mouseResizeWindow w
     --                                   >> windows W.shiftMaster))
