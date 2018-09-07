257f0768e25c3295792220988c3b39aa1de4078d
Grégoire Seux <g.seux@criteo.com>
2018-09-07 21:01:40 +0200

    Improve delocking mechanism to remove work-from-home status
    Change-Id: I604d6c527aef8b0b5f8acaf946e2ca5b57ada1ec

--- lock.sh
+++ lock.sh
@@ -4,9 +4,12 @@
 # find . -name "*.jpg" -exec mogrify -format png {} \;
 file=$(ls $HOME/img/*png| sort -R| head -n1)
 
+this_dir=$(dirname $0)
+
 revert() {
   xset dpms 0 0 3600
   amixer sset Master unmute
+  $this_dir/work_from_home.sh off
 }
 trap revert SIGHUP SIGINT SIGTERM
 xset +dpms dpms 120 180 600
