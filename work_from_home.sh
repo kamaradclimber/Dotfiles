c0db0b153c38bd9531409cdf5c5c7fd0da3c64af
Grégoire Seux <g.seux@criteo.com>
2018-09-23 14:30:02 +0200

    Improve work from home script
    - debuggable
    - set/restore emoji in status
    
    Change-Id: I051290485d62bb3b80d753ffb38a1d9b847fc330

--- work_from_home.sh
+++ work_from_home.sh
@@ -7,12 +7,21 @@ on_off=$1
 
 last_name="Seux"
 if [[ "$on_off" == "on" ]]; then
-  last_name="$last_name [work-from-home]"
-  echo "Activating work-from-home"
+  last_name="$last_name [work-at-home]"
+  emoji=":house:"
+  echo -n "Activating work-from-home..."
 else
-  echo "Disabling work-from-home"
+  echo -n "Disabling work-from-home...."
+  emoji=":rocket:"
 fi
 
 source ~/.watson_env
 
-curl -s -f -XPOST -H 'Content-Type: application/json' 'https://slack.com/api/users.profile.set' -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"profile\": {\"first_name\": \"Grégoire\", \"last_name\": \"$last_name\"}}" | jq .ok
+curl -s -f -XPOST -H 'Content-Type: application/json' 'https://slack.com/api/users.profile.set' -H "Authorization: Bearer $SLACK_TOKEN" -d "{\"profile\": {\"first_name\": \"Grégoire\", \"last_name\": \"$last_name\", \"status_emoji\": \"$emoji\"}}" > /tmp/work-from-home.result
+
+if [[ "$(cat /tmp/work-from-home.result | jq .ok)" == "true" ]]; then
+  echo "OK"
+else
+  echo "FAILED"
+  cat /tmp/work-from-home.result
+fi
