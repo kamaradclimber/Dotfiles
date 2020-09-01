6b4ca9947236d1f36f5480944c3c0326f68b8025
Grégoire Seux <kamaradclimber@gmail.com>
2020-09-01 09:13:30 +0200

    Filter candidates wallpaper
    It will now simply filter based on resolution
    
    Change-Id: I2422caf8d6621ebb20f4a586d06583417cb3471c

--- set-wallpaper.sh
+++ set-wallpaper.sh
@@ -2,4 +2,27 @@
 
 source=https://www.reddit.com/r/EarthPorn+ImaginaryLandscapes+ImaginaryTechnology+futureporn.json
 
-curl -s -L -H 'User-Agent: custom-timer' "$source" | jq '.data.children[] |.data.url' | head -100 | sort -R | xargs feh --bg-fill
+curl -s -L -H 'User-Agent: custom-timer' "$source" | jq '.data.children' > children.json
+
+
+cat <<EOR > tmp.script
+#!/usr/bin/env ruby
+
+require 'json'
+require 'pp'
+
+def extract_source(post)
+  return unless post['data']['preview']
+
+  source = post['data']['preview']['images'].find { |image| image['source'] }
+  source['source']
+end
+
+JSON
+  .parse(File.read('children.json'))
+  .select { |d| d['data']['preview'] }
+  .select { |post| extract_source(post) && extract_source(post)['width'] > 1920 }
+  .each { |post| puts post['data']['url'] }
+EOR
+
+ruby tmp.script| head -100 | sort -R | xargs feh --bg-fill
