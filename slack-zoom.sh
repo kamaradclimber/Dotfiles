b1b05a7e274530bf74145d9435c67eef8f67e5b6
Grégoire Seux <kamaradclimber@gmail.com>
2020-10-23 16:10:22 +0200

    Improve slack-zoom script
    Improve detection and detect more activities
    
    Change-Id: I6a4da68e4cfa323331de8d87ecf92b88d6aad4d2

--- slack-zoom.sh
+++ slack-zoom.sh
@@ -2,12 +2,21 @@
 
 echo "Detecting if running a zoom meeting"
 
-current_window=$(xdotool getwindowfocus getwindowname)
+current_window=$(xdotool getactivewindow getwindowname)
 
-echo "Current window is: $current_window"
+current_window_class=$(xprop -id $(xdotool getactivewindow) | grep -i class | cut -d = -f2  | awk '{print $1}' | sed -re 's/"(.*)".*/\1/')
 
-if [[ "$current_window" == "Zoom" ]]; then
-  ssh g_seux@churchill.criteois.lan '.dotfiles/work_from_home.sh zoom'
+echo "Current window is: $current_window, with class $current_window_class"
+
+function set_status {
+  status=$1
+  ssh g_seux@churchill.criteois.lan ".dotfiles/work_from_home.sh $status"
+}
+
+if [[ "$current_window" == "Zoom Meeting" ]]; then
+  set_status "zoom"
+elif [[ "$current_window_class" == "kitty" ]]; then
+  set_status "terminal"
 else
-  echo "Not in a zoom meeting"
+  echo "Not in a known type of window, will not do anything"
 fi
