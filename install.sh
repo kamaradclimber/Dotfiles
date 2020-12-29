299a067532bc1811a8e4c0481f790d260e5fa35c
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-29 16:21:11 +0100

    Stow most directories

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 
-if ! which stow 2> /dev/null; then
+if ! which stow > /dev/null 2>&1; then
   echo You must install GNU stow
   exit 1
 fi
 
-stow --dotfiles bash
-stow --dotfiles awesome
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+find . -maxdepth 1 -mindepth 1 -type d -regex "\./[^.].+" |
+  grep -v -e misc$ |
+  while read prog; do
+  prog=$(echo $prog | cut -f2 -d/)
+  echo "Will use stow on $prog"
+  stow --dotfiles $prog
+done
