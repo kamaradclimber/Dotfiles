fd55a39aed8aa120fd6db1e68a650212183dbc25
Grégoire Seux <g.seux@criteo.com>
2020-12-29 17:57:34 +0100

    Add setup script for wallpaper

--- install.sh
+++ install.sh
@@ -13,5 +13,11 @@ find . -maxdepth 1 -mindepth 1 -type d -regex "\./[^.].*" |
   while read prog; do
   prog=$(echo $prog | cut -f2 -d/)
   echo "Will use stow on $prog"
-  stow --dotfiles $prog --verbose 2
+  stow --dotfiles $prog --verbose 2 --ignore=setup
+  if test -f $prog/setup; then
+    echo "Running setup script"
+    pushd $prog
+    ./setup
+    popd
+  fi
 done
