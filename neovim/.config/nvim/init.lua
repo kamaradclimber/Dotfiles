140267e2c346fec324af4a0231a8b2ef1bba47c7
Grégoire Seux <g.seux@criteo.com>
2021-08-09 19:56:20 +0200

    Rewrite nvim config in lua
    Change-Id: I27bfb81554ada4e70b5912c0a2ae32c8f43866d1

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,86 @@
+-- Package manager, use PaqInstall to install :)
+require "paq" {
+	"savq/paq-nvim";                  -- Let Paq manage itself
+	'neomake/neomake'; --linting
+	'janko-m/vim-test'; -- test for ruby
+	'peitalin/vim-jsx-typescript'; --typescript
+	{'nvim-treesitter/nvim-treesitter', run=':TSUpdate' };
+	'nvim-treesitter/playground';
+	'neovim/nvim-lspconfig'; -- language server
+	'rcarriga/nvim-notify';
+}
+
+vim.api.nvim_exec("call neomake#configure#automake('nrwi', 500)", false)
+
+local map = vim.api.nvim_set_keymap
+
+-- map method takes 4 arguments: the mode for which the mapping will take effect, the key sequence, the command to execute and a table of options
+map('n', ',', '', {})
+vim.g.mapleader = ','  -- 'vim.g' sets global variables
+
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
+
+o.ignorecase = true -- when searching, ignore case
+o.number = true -- display line numbers
+
+o.tabstop = 2 -- number of visual spaces per tab
+o.softtabstop = 2 -- number of spaces in tab when editing
+o.shiftwidth = 2 -- number of spaces to use for autoindent
+o.expandtab = true -- tabs are spaces
+o.autoindent = true
+o.copyindent = true -- copy indent from previous line
+
+local cmd = vim.cmd
+cmd("command W w !sudo tee % > /dev/null")
+
+options = { noremap = true }
+map('n', '<leader>t', ':TestNearest<cr>', options)
+map('n', '<leader>T', ':TestFile<cr>', options)
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = {}, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = {},  -- list of language that will be disabled
+  },
+  indent = {
+    enable = true
+  }
+}
+
+require'lspconfig'.solargraph.setup{}
+require'lspconfig'.pyright.setup{}
+
+local nvim_lsp = require('lspconfig')
+
+-- Use an on_attach function to only map the following keys 
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { "pyright", "rust_analyzer", "tsserver" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
