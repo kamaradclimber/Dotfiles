27ec3cf9b1bf3a0cc61f1b99abd78787a8dc0db8
Grégoire Seux <g.seux@criteo.com>
2021-10-19 10:29:14 +0200

    Move history files
    Change-Id: I2e1b68309d2517e36ba7c0a75f61f3f3a4f4f35c

--- bash/dot-custom_history.sh
+++ bash/dot-custom_history.sh
@@ -2,7 +2,7 @@
 __fzf_history__() {
   local line
   line=$(
-  HISTTIMEFORMAT= command cat ~/.bash-history-* |
+  HISTTIMEFORMAT= command cat ~/.bash_history_storage/* |
     FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS --tac --sync -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS +m" $(__fzfcmd)) &&
   line=$(awk '{ print substr($0, index($0,$4)) }' <<< "$line")
 
