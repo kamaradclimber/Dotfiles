f4c26cec6adc79c5bc9b6d07c85ce8d793c266ba
Grégoire Seux <gregoire.seux@datadoghq.com>
2024-12-06 11:54:39 +0100

    Slow implementation of alt-tab for aerospace

---
+++ macos-wm/.config/aerospace/alt-tab.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# simple implementation of alt-tab
+# sadly it does not display the list windows
+
+list=$(aerospace list-windows --workspace focused | awk '{print $1}')
+current_window_id=$(aerospace list-windows --focused | awk '{print $1}')
+# echo "Current window id: $current_window_id"
+# echo "List of windows: $list"
+
+next=$(echo -e "$list\n$list" | grep -A 1 $current_window_id | head -n2 | tail -n1)
+# echo "Next window id: $next"
+aerospace focus --window-id $next
