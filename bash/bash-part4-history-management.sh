22ddc6a833f24f29e2343f0ece61495c6716f546
Grégoire Seux <gregoire.seux@datadoghq.com>
2025-02-11 17:22:32 +0100

    Migrate history to atuin

--- bash/bash-part4-history-management.sh
+++ bash/bash-part4-history-management.sh
@@ -4,10 +4,3 @@ export HISTFILESIZE=100000 #commands in the history file
 export HOSTSIZE=10000 #commands remembered by one shell
 export HISTCONTROL=ingorespace:erasedups
 shopt -s histappend
-
-mkdir -p .bash_history_storage
-find . -maxdepth 1 -name ".bash-history-*.log" | while read file; do
-  echo One shot move of $file
-  new_name=$(echo $file | awk  -F "/" '{print $NF}' | sed 's/.bash-history-//')
-  mv $file .bash_history_storage/$new_name
-done
