ef9cd47ea82effd1549ceb01e1145e8fd1ab20b8
Grégoire Seux <gregoire.seux@datadoghq.com>
2025-05-28 07:52:03 +0200

    Add neovim setup

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ ln -sf dotfiles .dotfiles
 rm -f ~/.bashrc
 
 cd .dotfiles
-for prog in bash git; do
+for prog in bash git neovim; do
   echo "Will use stow on $prog"
   stow --dotfiles $prog --verbose 2 --ignore=setup
   if test -f $prog/setup; then
