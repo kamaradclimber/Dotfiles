327feb7734c87466966a10fe9d62f525d63a77f8
Grégoire Seux <gregoire.seux@datadoghq.com>
2026-02-10 17:49:08 +0100

    Speed up path building for ruby

--- bash/bash-part5-path.sh
+++ bash/bash-part5-path.sh
@@ -4,18 +4,24 @@ export PATH=$PATH:~/.dotfiles/scripts/
 export PATH="/Users/gregoire.seux/.rbenv/shims/:$PATH"
 
 # classical method is to run `ruby -e 'puts Gem.user_dir'` and add the bin directory to the PATH
-# however, invocation of ruby costs 100ms on macos, so let’s find a better technique
+# however, invocation of ruby costs 100ms on macos, so let's use a cached approach
 LIKELY_RUBY_HOME=$HOME/.gem/ruby
-if [ -d $LIKELY_RUBY_HOME ]; then
-  RUBY_VERSION=$(command ls -d $LIKELY_RUBY_HOME/*/bin | sort | tail -n 1 | sed -re "s|/bin||"| sed -re "s|.+/||")
-  export GEM_HOME=$RUBY_VERSION
+if [ -d "$LIKELY_RUBY_HOME" ]; then
+  # Use bash globbing instead of ls+sort+tail+sed (much faster)
+  # This finds the latest ruby version directory
+  shopt -s nullglob
+  ruby_dirs=("$LIKELY_RUBY_HOME"/*/bin)
+  if [ ${#ruby_dirs[@]} -gt 0 ]; then
+    # Get the last one (highest version due to alphabetical sorting)
+    latest_ruby_bin="${ruby_dirs[-1]}"
+    # Extract just the version number using bash parameter expansion
+    RUBY_VERSION="${latest_ruby_bin%/bin}"
+    RUBY_VERSION="${RUBY_VERSION##*/}"
+    export GEM_HOME="$LIKELY_RUBY_HOME/$RUBY_VERSION"
+    export PATH="$PATH:$GEM_HOME/bin"
+  fi
+  shopt -u nullglob
 fi
-if [[ "$RUBY_VERSION" == "" ]]; then
-  export GEM_HOME=$(ruby -e 'puts Gem.user_dir')
-fi
-
-RUBY_PATH=$GEM_HOME/bin
-export PATH="$PATH:$RUBY_PATH"
 
 
 if [ -d "$HOME/.local/bin/" ]; then
